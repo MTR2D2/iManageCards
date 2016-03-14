5ca18f6f019c6a67d6d75d5767ed2fc06458906f
Michael Reynolds <reynoldsmichael945@gmail.com>
2016-03-14 00:13:19 -0400

    Initial Yak Shaving

---
+++ iManageCards/iManageCards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iManageCards
+//
+//  Created by Michael Reynolds on 3/14/16.
+//  Copyright © 2016 Simplicity, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
